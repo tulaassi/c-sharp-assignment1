8129b6bc4412fd027068d016c3eb9d5d4ddc12b2
tulaassi <115005243+tulaassi@users.noreply.github.com>
2022-11-13 14:08:57 +0530

    assignment 6

--- csharpassignment/Program.cs
+++ csharpassignment/Program.cs
@@ -1,4 +1,81 @@
-﻿
+﻿// assignment 6 based on structure
+ internal class csharpQ6
+    {
+        int bookid;
+        string _title;
+        float _price;
+        int booktype;
+        public int Id
+        {
+            get { return bookid; }
+            set { bookid = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public float Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public Type Booktype
+        {
+            get { return (Type)booktype; }
+            set { booktype = (int)value; }
+        }
+        public void print()
+        {
+            Console.WriteLine("Your Book Id={0} Title={1} Price={2} Type={3}", bookid, _title, _price, booktype);
+        }
+        public enum Type
+        {
+            magazine,
+            novel,
+            referencebook,
+            miscelleous
+        }
+        class Bookcall
+
+        {
+            public static void Main()
+            {
+                try
+                {
+                    csharpQ6 b1 = new csharpQ6();
+                    b1.Id = 1;
+                    b1.Title = "Changed History";
+                    b1.Price = 860.44F;
+                    b1.Booktype = Type.miscelleous;
+                    b1.print();
+
+                    csharpQ6 b2 = new csharpQ6();
+                    b2.Id = 2;
+                    b2.Title = "CSharp";
+                    b2.Price = 763.21F;
+                    b2.Booktype = Type.referencebook;
+                    b2.print();
+
+                    csharpQ6 b3 = new csharpQ6();
+                    b3.Id = 3;
+                    b3.Title = "Empty Pocket";
+                    b3.Price = 899.23F;
+                    b3.Booktype = Type.magazine;
+                    b3.print();
+                }
+                catch (Exception Error)
+                {
+                    Console.WriteLine(Error.ToString());
+                }
+                finally
+                {
+                    Console.WriteLine("It was final");
+                }
+            }
+        }
+    }
+}
 
 
 
